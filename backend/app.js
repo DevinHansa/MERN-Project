042e44ad0da22e87836a5dbdd18f48c78915ed1c
DevinHansa <devinhansa@gmail.com>
2023-12-22 17:15:24 +0530

    call back

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const controller = require('./controller');
+
 
 app.use(cors());
 
@@ -13,6 +15,19 @@ app.use(
 
 app.use(express.json());
 
+app.get('/users', (req, res) => {
+    var resObj = [];
+    controller.getUsers(users => {
+        res.send(users);
+    });
+});
+
+app.get('/user', (req, res) => {
+    const id = req.query.id;
+    controller.getUserbyId(id, user => {
+        res.send(user);
+    });
+});
 
 
 module.exports = app;
